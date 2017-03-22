fa3ec22847e055163101c0e4d51653118fda7535
unknown <>
2017-03-22 20:00:36 +0800

    new pro

---
+++ app/src/main/java/com/example/liujiang/testone/acp/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.liujiang.testone.acp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liujiang on 2017/3/22.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
